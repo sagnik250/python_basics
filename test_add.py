5aa79614541b13d0f46f9ec27d147c1fcca339e4
sagnik250 <sagnik250@gmail.com>
2017-12-04 13:28:15 +0530

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -1,9 +1,4 @@
-#from test_arithmetic import arithmetic
-class arithmetic:
-    def __init__(self, x, y):
-        #x and y are the operands to an arithmetic class of operation
-        self.x = x
-        self.y = y
+from test_arithmetic import arithmetic
 
 class add(arithmetic):
 
